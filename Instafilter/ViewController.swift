2411886c70f2261f4d41d1fac6fb1ff1586142d0
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2021-12-31 22:51:00 +0300

    Added image picker controller and protocols

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -7,18 +7,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
   //MARK: - Properties
   @IBOutlet var imageView: UIImageView!
   @IBOutlet var intensity: UISlider!
+  var currentImage: UIImage!
 
   //MARK: - ViewController lificycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    title = "Instafilter"
+    navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(importPicture))
   }
 
+  //MARK: - Methods
+
+  func importPicture(){
+    let picker = UIImagePickerController()
+    picker.allowsEditing = true
+    picker.delegate = self
+    present(picker, animated: true)
+  }
+
+  func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    guard let image = info[.editedImage] as? UIImage else { return }
+    dismiss(animated: true)
+    currentImage = image
+  }
 
   //MARK: Actions
   @IBAction func intensityChanged(_ sender: Any) {
