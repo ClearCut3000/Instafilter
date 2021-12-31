dc6dc02473d5164124db2d55c9eca0cf4e0c3730
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2021-12-31 22:32:44 +0300

    Initial commit. Main UI added

--- Instafilter/ViewController.swift
+++ Instafilter/ViewController.swift
@@ -9,11 +9,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  //MARK: - Properties
+  @IBOutlet var imageView: UIImageView!
+  @IBOutlet var intensity: UISlider!
+
+  //MARK: - ViewController lificycle
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
 
+
+  //MARK: Actions
+  @IBAction func intensityChanged(_ sender: Any) {
+  }
+
+  @IBAction func changeFilter(_ sender: Any) {
+  }
+
+  @IBAction func save(_ sender: Any) {
+  }
 
 }
 
